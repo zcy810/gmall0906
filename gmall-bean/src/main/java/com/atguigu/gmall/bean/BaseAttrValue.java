3a6a50c9ebe4813e3380e774ad9313b6039c042e
zcy <792898450@qq.com>
2019-01-15 14:06:05 +0800

    3

--- gmall-bean/src/main/java/com/atguigu/gmall/bean/BaseAttrValue.java
+++ gmall-bean/src/main/java/com/atguigu/gmall/bean/BaseAttrValue.java
@@ -33,6 +33,7 @@ public class BaseAttrValue implements Serializable {
                 '}';
     }*/
 
+
     public String getValueName() {
         return valueName;
     }
---
+++ gmall-bean/src/main/java/com/atguigu/gmall/bean/SpuSaleAttr.java
@@ -0,0 +1,77 @@
+package com.atguigu.gmall.bean;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+public class SpuSaleAttr implements Serializable {
+    @Id
+    @Column
+    private String id ;
+
+    @Column
+    private String spuId;
+
+    @Column
+    private String saleAttrId;
+
+    @Column
+    private String saleAttrName;
+
+
+    @Transient
+    private List<SpuSaleAttrValue> spuSaleAttrValueList;
+
+    @Transient
+    private Map spuSaleAttrValueJson;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSpuId() {
+        return spuId;
+    }
+
+    public void setSpuId(String spuId) {
+        this.spuId = spuId;
+    }
+
+    public String getSaleAttrId() {
+        return saleAttrId;
+    }
+
+    public void setSaleAttrId(String saleAttrId) {
+        this.saleAttrId = saleAttrId;
+    }
+
+    public String getSaleAttrName() {
+        return saleAttrName;
+    }
+
+    public void setSaleAttrName(String saleAttrName) {
+        this.saleAttrName = saleAttrName;
+    }
+
+    public List<SpuSaleAttrValue> getSpuSaleAttrValueList() {
+        return spuSaleAttrValueList;
+    }
+
+    public void setSpuSaleAttrValueList(List<SpuSaleAttrValue> spuSaleAttrValueList) {
+        this.spuSaleAttrValueList = spuSaleAttrValueList;
+    }
+
+    public Map getSpuSaleAttrValueJson() {
+        return spuSaleAttrValueJson;
+    }
+
+    public void setSpuSaleAttrValueJson(Map spuSaleAttrValueJson) {
+        this.spuSaleAttrValueJson = spuSaleAttrValueJson;
+    }
+}
